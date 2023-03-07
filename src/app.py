5ff2836899858a5a86e1d7c4008621d5c2ec61dd
datrelles <desarrollador2@massline.com.ec>
2023-03-07 11:31:15 -0500

    modificacion main

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
+import json
+
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from config import config
 from src import oracle
+import numpy
 
 app=Flask(__name__)
 
@@ -8,14 +11,28 @@ app=Flask(__name__)
 def index():
     return redirect(url_for('login'))
 
-@app.route('/my-first-api', methods = ['GET'])
+@app.route('/api-packing-list', methods = ['GET'])
 def hello():
+    class create_dict(dict):
 
-    name = request.args.get('name')
-    return jsonify({
-        # "message": "PackingList Data",
-        "Packing List data": oracle.execute_sql('select COD_PRODUCTO, COD_MOTOR, COD_CHASIS, CAMVCPN, ANIO, COD_COLOR, CILINDRAJE, TONELAJE, OCUPANTES, MODELO, CLASE, SUBCLASE, FECHA_ADICION, FECHA_MODIFICACION from ST_PROD_PACKING_LIST where rownum<1000')
-        })
+        # __init__ function
+        def __init__(self):
+            self = dict()
+
+            # Function to add key:value
+
+        def add(self, key, value):
+            self[key] = value
+
+    year = request.args.get('year')
+    array = oracle.execute_sql(
+        'select ROWNUM, P.COD_PRODUCTO, P.COD_MOTOR, P.COD_CHASIS, P.CAMVCPN, P.ANIO, P.COD_COLOR, P.CILINDRAJE, P.TONELAJE, P.OCUPANTES, P.MODELO, P.CLASE, P.SUBCLASE, P.FECHA_ADICION, P.FECHA_MODIFICACION, V.COD_LIQUIDACION, V.NOMBRE '
+        'from ST_PROD_PACKING_LIST P, VT_VALORACION_SERIE V where P.COD_MOTOR = V.NUMERO_SERIE AND P.ANIO = '+year)
+    mydict = create_dict()
+    for row in array:
+        mydict.add(row[0],({"CODPRODUC":row[1],"CODMOTOR":row[2],"CODCHASIS":row[3], "CPN":row[4], "YEAR":row[5],"COLOR":row[6],"CILINDRAJE":row[7],"TONELAJE":row[8], "OCUPANTES":row[9], "MODELO":row[10],"CLASE":row[11], "SUBCLASE":row[12], "FECHA CREACION":row[13], "FECHA MODIFICACION":row[14], "CODIGO LIQUIDACION":row[15], "IMPORTACION":row[16]}))
+    stud_json = json.dumps(mydict, indent=2, default=str)
+    return stud_json
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -27,5 +44,6 @@ def login():
         return render_template('auth/login.html')
 
 if __name__=='__main__':
-    app.config.from_object(config['development'])
-    app.run()
+    # app.config.from_object(config['development'])
+    app.run(host='0.0.0.0',port=5000)
+
