d9a97f1f9ddbc7a4ee9b6de15b97a910bd29c7ab
ismacalle <desarrollador1@massline.com.ec>
2023-08-01 15:17:26 -0500

    [FIX]src en models tipo_aforo

--- src/models/tipo_aforo.py
+++ src/models/tipo_aforo.py
@@ -2,7 +2,7 @@
 from sqlalchemy import Column, DateTime, VARCHAR
 from sqlalchemy.dialects.oracle import NUMBER
 from sqlalchemy.ext.declarative import declarative_base
-from config.database import db
+from src.config.database import db
 
 Base = declarative_base(metadata = db.metadata)
 
