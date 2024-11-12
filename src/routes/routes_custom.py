f83b41532d751da1dc7bac572c9edb05cbb3aa54
datrelles <desarrollador2@massline.com.ec>
2024-11-12 17:06:49 -0500

    Fix Formulas - lote por instancia de lote

--- src/routes/routes_custom.py
+++ src/routes/routes_custom.py
@@ -1793,53 +1793,52 @@ def generate_combo():
 
         ############################################CREACION DE LOTE PARA FORMULA######################################################################
 
-        query = """
-                                    DECLARE
-                                      v_cod_empresa           FLOAT := :1;
-                                      v_cod_agencia           FLOAT := :2;
-                                      v_tipo_comprobante_lote  VARCHAR2(50) := :3;
-                                      v_tipo_lote             VARCHAR2(3) := :4;  
-                                      v_result                VARCHAR2(50);
-                                    BEGIN
-                                      v_result := ks_lote.asigna_codigo(p_empresa => v_cod_empresa,
-                                                                                  p_cod_agencia => v_cod_agencia,
-                                                                                  p_tipo_comprobante_lote => v_tipo_comprobante_lote,
-                                                                                  p_fecha => sysdate,
-                                                                                  P_TIPO_LOTE => v_tipo_lote);
-                                    :5 := v_result;
-                                    END;
-                                    """
-        cur = db1.cursor()
-        result_var = cur.var(cx_Oracle.STRING)
-        cur.execute(query, (empresa, cod_agencia, 'LT', 'IN', result_var))
-        cod_comprobante_lote_formula = result_var.getvalue()
-        cur.close()
-        db1.commit()
+        # query = """
+        #                             DECLARE
+        #                               v_cod_empresa           FLOAT := :1;
+        #                               v_cod_agencia           FLOAT := :2;
+        #                               v_tipo_comprobante_lote  VARCHAR2(50) := :3;
+        #                               v_tipo_lote             VARCHAR2(3) := :4;
+        #                               v_result                VARCHAR2(50);
+        #                             BEGIN
+        #                               v_result := ks_lote.asigna_codigo(p_empresa => v_cod_empresa,
+        #                                                                           p_cod_agencia => v_cod_agencia,
+        #                                                                           p_tipo_comprobante_lote => v_tipo_comprobante_lote,
+        #                                                                           p_fecha => sysdate,
+        #                                                                           P_TIPO_LOTE => v_tipo_lote);
+        #                             :5 := v_result;
+        #                             END;
+        #                             """
+        # cur = db1.cursor()
+        # result_var = cur.var(cx_Oracle.STRING)
+        # cur.execute(query, (empresa, cod_agencia, 'LT', 'IN', result_var))
+        # cod_comprobante_lote_formula = result_var.getvalue()
+        # cur.close()
+        # db1.commit()
+        #
+        # query = StLote.query()
+        # if empresa:
+        #     query = query.filter(StLote.empresa == empresa)
+        # if cod_agencia:
+        #     query = query.filter(StLote.cod_agencia == cod_agencia)
+        # if cod_comprobante_lote_formula:
+        #     query = query.filter(StLote.cod_comprobante == cod_comprobante_lote_formula)
+        #
+        # result = query.all()
 
-        query = StLote.query()
-        if empresa:
-            query = query.filter(StLote.empresa == empresa)
-        if cod_agencia:
-            query = query.filter(StLote.cod_agencia == cod_agencia)
-        if cod_comprobante_lote_formula:
-            query = query.filter(StLote.cod_comprobante == cod_comprobante_lote_formula)
-
-        result = query.all()
-
-        if not result:
-            lote = StLote(
-                empresa=empresa,
-                tipo_comprobante='LT',
-                cod_comprobante=cod_comprobante_lote_formula,
-                fecha=date.today(),
-                descripcion='Lote para creacion de combos',
-                tipo_lote='IN',
-                cod_agencia=cod_agencia,
-                usuario_aud=usuario,
-                fecha_aud=date.today()
-            )
-            db.session.add(lote)
-            db.session.commit()
+        lote = StLote(
+            empresa=empresa,
+            tipo_comprobante='LT',
+            cod_comprobante=cod_comprobante,
+            fecha=date.today(),
+            descripcion='Lote para creacion de combos',
+            tipo_lote='IN',
+            cod_agencia=cod_agencia,
+            usuario_aud=usuario,
+            fecha_aud=date.today()
+        )
+        db.session.add(lote)
+        db.session.commit()
 
         query = """
                                            DECLARE
@@ -1889,7 +1888,7 @@ def generate_combo():
                                                     ORDER BY 
                                                         L.Fecha_Ingreso DESC 
                                                                     """,
-                       param1=cod_agencia, param2=empresa, param3=cod_producto, param4=cod_comprobante_lote_formula)
+                       param1=cod_agencia, param2=empresa, param3=cod_producto, param4=cod_comprobante)
 
         existencia_lote = cursor.fetchall()
         cursor.close()
@@ -1922,7 +1921,7 @@ def generate_combo():
                 transportador=None,
                 placa=None,
                 tipo_comprobante_lote='LT',
-                cod_comprobante_lote=cod_comprobante_lote_formula,
+                cod_comprobante_lote=cod_comprobante,
                 cod_comprobante_ingreso=None,
                 tipo_comprobante_ingreso=None,
                 tipo_identificacion_transporta=None,
@@ -1943,7 +1942,7 @@ def generate_combo():
                 cod_estado_producto=None,
                 ubicacion_bodega=None,
                 cod_tipo_lote='LT',
-                cod_comprobante_lote=cod_comprobante_lote_formula,
+                cod_comprobante_lote=cod_comprobante,
                 cod_estado_producto_ing=None,
                 cantidad_pedida=None
             )
@@ -2357,7 +2356,7 @@ def generate_combo():
             ubicacion_bodega=None,
             cantidad_promocion=None,
             tipo_comprobante_lote='LT',
-            cod_comprobante_lote=cod_comprobante_lote_formula,
+            cod_comprobante_lote=cod_comprobante,
             descuento_regalo=None,
             precio_unitario_xml=None,
             descuento_xml=None,
