21cf3b6a35cf3fc915309f95d2abb782e2f97946
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2025-06-24 16:53:27 -0500

    Especificar mensaje con error bd 0

--- src/decorators/exception.py
+++ src/decorators/exception.py
@@ -37,7 +37,7 @@ def handle_exceptions(action):
                             if not part.startswith('ORA-'):
                                 mensaje = part.split('\n')[0]
                                 break
-                    elif error_code == 12571:
+                    elif error_code == 0:
                         mensaje = "Se perdió conexión con la base de datos"
                 logger.exception(f'Ocurrió una excepción con la base de datos al {action}: {e}')
                 return jsonify({'mensaje': mensaje}), status_code
